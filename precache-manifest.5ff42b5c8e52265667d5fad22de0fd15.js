e42cd297d1327c7f4a593183884ab03fa1aa92db
Iwo Plaza <iwoplaza@gmail.com>
2020-01-26 13:43:11 +0100

    Updates

---
+++ precache-manifest.5ff42b5c8e52265667d5fad22de0fd15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61168d63b2623cf1f4e1c4d0ccea3919",
+    "url": "/uni-learn/index.html"
+  },
+  {
+    "revision": "ee6df023c117f96f8196",
+    "url": "/uni-learn/static/css/main.71f47fc0.chunk.css"
+  },
+  {
+    "revision": "fee70ce70ece8e1acabe",
+    "url": "/uni-learn/static/js/2.f2b2b86a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/uni-learn/static/js/2.f2b2b86a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee6df023c117f96f8196",
+    "url": "/uni-learn/static/js/main.e0278239.chunk.js"
+  },
+  {
+    "revision": "3bcc7859c4e0c39a48df",
+    "url": "/uni-learn/static/js/runtime-main.bbaff127.js"
+  }
+]);
